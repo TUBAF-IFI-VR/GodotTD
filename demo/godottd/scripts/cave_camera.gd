25b52b95a306b26af14fde178f20da6a932310e9
Florian Richter <flo.richter@informatik.tu-freiberg.de>
2024-08-30 17:08:13 +0200

    Fixed and cleaned cave camera code

--- demo/godottd/scripts/cave_camera.gd
+++ demo/godottd/scripts/cave_camera.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@onready var cave : Node3D = get_node_or_null("/root/CAVE")
+@onready var cave : Node3D = GodotTD.tiled_display
 
 var camera_angle = 0.0
 var mouse_sensitivity = 0.001
@@ -16,7 +16,9 @@ var tilt_mode = -1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	if not GodotTD.is_server:
+		set_process(false)
+		set_process_input(false)
 
 func _input(event):
 	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
@@ -28,54 +30,23 @@ func _input(event):
 	
 
 func _physics_process(delta):
-	if !cave or !cave.is_server:
-		return
+	#if !cave or !cave.is_server:
+	#	return
+		
 	# Reset direction
 	direction = Vector3()
 	var aim = global_transform.basis.z
 	var side = global_transform.basis.x
-	#var up = global_transform.basis.y
 	
 	var target_speed = 1.0
-	#if Input.is_action_pressed("fast"):
-	#	target_speed *= 3.0
-	#if Input.is_action_pressed("superfast"):
-	#	target_speed *= 6.0
 	speed = speed + (target_speed - speed) * delta * 3.0 * MOVE_ACCEL;
 	
-	# Move forward and backward
-	#var rotate_multiplier = sqrt(speed)
-	#if Input.is_action_pressed("look_left"):
-	#	rotation.y += ROTATE_SPEED * rotate_multiplier
-	#elif Input.is_action_pressed("look_right"):
-	#	rotation.y -= ROTATE_SPEED * rotate_multiplier
-	#if Input.is_action_pressed("look_up"):
-	#	rotation.x = clamp(rotation.x + ROTATE_SPEED * rotate_multiplier * tilt_mode, -1.5, 1.5)
-	#elif Input.is_action_pressed("look_down"):
-	#	rotation.x = clamp(rotation.x - ROTATE_SPEED * rotate_multiplier * tilt_mode, -1.5, 1.5)
-	#if Input.is_action_just_released("reset_forward"):
-	#	rotation.x = 0.0
-	#if Input.is_action_just_released("reset_down"):
-	#	rotation.x = -1.5
-	#if Input.is_action_just_released("toggle_tiltmode"):
-	#	tilt_mode *= -1.0
-	
 	direction = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	
 	direction = (aim*direction.y) + (side*direction.x)
-	#if Input.is_action_pressed("move_up"):
-	#	direction += up
-	#if Input.is_action_pressed("move_down"):
-	#	direction -= up
-	
+
 	var target = direction * MOVE_SPEED * speed
 	
 	# accelerate
 	global_translate(Vector3().lerp(target, MOVE_ACCEL * delta));
 	cave.update_head_transform(transform)
-	
-	#velocity = velocity.linear_interpolate(target, MOVE_ACCEL * delta)
-	
-	# move the player
-	#translate(velocity)
 	
